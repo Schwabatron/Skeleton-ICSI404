66e2f5d104132d403eefc92f0caa42145a8ad9c8
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-02-02 14:13:43 -0500

    Word16 class completed. still need to add comments. will do that in a future branch all in one pr

--- src/Word16.java
+++ src/Word16.java
@@ -1,59 +1,112 @@
 public class Word16 {
+    private static final int size = 16;
+    private final Bit[] Word;
+
+
 
     public Word16() {
+        this.Word = new Bit[size];
+        for(int i = 0; i < size; i++)
+        {
+            this.Word[i] = new Bit(false);
+        }
     }
 
-    public Word16(Bit[] in) {}
 
-    public void copy(Word16 result) { // sets the values in "result" to be the same as the values in this instance; use "bit.assign"
+
+    public Word16(Bit[] in) {
+        this.Word = new Bit[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            this.Word[i] = in[i];
+        }
     }
 
-    public void setBitN(int n, Bit source) { // sets the nth bit of this word to "source"
+    public void copy(Word16 result) {// sets the values in "result" to be the same as the values in this instance; use "bit.assign"
+        for (int i = 0; i < size; i++)
+        {
+
+            result.Word[i].assign(this.Word[i].getValue());
+        }
+    }
+
+    public void setBitN(int n, Bit source) {// sets the nth bit of this word to "source"
+        this.Word[n] = source;
     }
 
     public void getBitN(int n, Bit result) { // sets result to be the same value as the nth bit of this word
+        result.assign(this.Word[n].getValue());
     }
 
-    public boolean equals(Word16 other) { // is other equal to this
-        return false;
+    public boolean equals(Word16 other) {// is other equal to this
+        return equals(this, other);
     }
 
     public static boolean equals(Word16 a, Word16 b) {
-        return false;
+
+        for(int i = 0; i < size; i++)
+        {
+            if(a.Word[i].getValue() != b.Word[i].getValue())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void and(Word16 other, Word16 result) {
+        and(this, other, result);
     }
 
     public static void and(Word16 a, Word16 b, Word16 result) {
+        for(int i = 0; i < size; i++)
+        {
+            a.Word[i].and(b.Word[i], result.Word[i]);
+        }
     }
 
     public void or(Word16 other, Word16 result) {
+        or(this, other, result);
     }
 
     public static void or(Word16 a, Word16 b, Word16 result) {
+        for(int i = 0; i < size; i++)
+        {
+            a.Word[i].or(b.Word[i], result.Word[i]);
+        }
     }
 
     public void xor(Word16 other, Word16 result) {
+        xor(this, other, result);
     }
 
     public static void xor(Word16 a, Word16 b, Word16 result) {
+        for(int i = 0; i < size; i++)
+        {
+            a.Word[i].xor(b.Word[i], result.Word[i]);
+        }
     }
 
     public void not( Word16 result) {
+        not(this, result);
     }
 
     public static void not(Word16 a, Word16 result) {
+        for(int i = 0; i < size; i++)
+        {
+            a.Word[i].not(result.Word[i]);
+        }
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        /*  -- match this format
-        for (Bit bit : bits) {
+
+        for (Bit bit : Word) {
             sb.append(bit.toString());
             sb.append(",");
         }
-         */
+
         return sb.toString();
     }
 }
