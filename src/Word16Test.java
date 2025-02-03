7f1abbf2421fe770cf89c47c629d445d97f085ba
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-02-03 15:43:28 -0500

    test change

--- src/Word16Test.java
+++ src/Word16Test.java
@@ -116,10 +116,9 @@ class Word16Test {
     void copy_test() {
         Word16 w = getStripe();
         Word16 w2 = new Word16(new Bit[]{ t, t, f, f, t, t, f, f, t, t, f, f, t, t, f, f });
-        var result = new Word16();
         w.copy(w2);
-        System.out.println(w.toString());
-        System.out.println(w2.toString());
+        System.out.println(w);
+        System.out.println(w2);
         assertTrue(w.equals(w2));
     }
 }
