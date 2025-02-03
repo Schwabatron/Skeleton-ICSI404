95ed02da9da00e8b1896de0e8ffc57130cb2c51e
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-02-03 14:53:16 -0500

    Documented the Bit class with javadoc style comments

--- src/Bit.java
+++ src/Bit.java
@@ -1,42 +1,57 @@
+
+/**
+ * The {@code Bit} class represents a single bit that can be either {@code TRUE} or {@code FALSE}.
+ * It provides the following logical operations: AND, OR, XOR, and NOT.
+ */
 public class Bit {
+
+    /**
+     * Enum representing possible boolean values for the {@code Bit} class.
+     */
     public enum boolValues { FALSE, TRUE }
 
-    private boolValues single_bit;//Private member representing the bit we are working with
+    /**
+     * Private member representing the bit we are working with
+     */
+    private boolValues single_bit;
 
 
 
-    /*
-    Constructor for the Bit class
-    Parameters: Boolean value
-    Using the parameter value the constructor will check the boolean value
-    if the boolean value is true the bit will be assigned to TRUE in the boolValues enum
-    if the boolean value is false the bit will be assigned to FALSE in the boolValues enum
+    /**
+     * Constructor for the {@code Bit} class.
+     * Takes a bool parameter and initializes the bit value depending on the value.
+     *
+     * @param value {@code true} sets the bit to {@code boolValues.TRUE}, {@code false} sets it to {@code boolValues.FALSE}
      */
     public Bit(boolean value) {
-        if(value) { //If the value is true
-            this.single_bit = boolValues.TRUE; //The bit is TRUE
+        if(value) {
+            this.single_bit = boolValues.TRUE;
         }
         else {
-            this.single_bit = boolValues.FALSE; //If false the bit is FALSE
+            this.single_bit = boolValues.FALSE;
         }
     }
 
-    /*
-    getValue method
-    Parameters: N/A
-    the getValue method is a simple getter method that will simply return the enum contained
-    in the single_bit variable (either boolValues.TRUE or boolValues.FALSE)
+    /**
+     * Accessor method to find the current value of the bit
+     *
+     * @return The current {@code boolValues} stored in {@code single_bit}, either {@code TRUE} or {@code FALSE}.
      */
     public boolValues getValue() {
         return single_bit;
     }
 
+    /**
+     * Assigns a new value to the bit
+     *
+     * @param value The new {@code boolValues} to assign ({@code TRUE} or {@code FALSE}).
+     */
     public void assign(boolValues value) {
-        if(value == boolValues.TRUE) { //If the value is true
-            single_bit = boolValues.TRUE; //The bit is TRUE
+        if(value == boolValues.TRUE) {
+            single_bit = boolValues.TRUE;
         }
         else {
-            single_bit = boolValues.FALSE; //If false the bit is FALSE
+            single_bit = boolValues.FALSE;
         }
     }
 
@@ -44,7 +59,13 @@ public class Bit {
        and(this, b2, result);
     }
 
-
+    /**
+     * Performs an AND operation between two bits and stores the result in a third bit.
+     *
+     * @param b1 The first {@code Bit} operand.
+     * @param b2 The second {@code Bit} operand.
+     * @param result The {@code Bit} where the result will be stored.
+     */
     public static void and(Bit b1, Bit b2, Bit result) {
         if((b1.getValue() == boolValues.TRUE) && (b2.getValue() == boolValues.TRUE)) {
             result.assign(boolValues.TRUE);
@@ -58,6 +79,13 @@ public class Bit {
         or(this, b2, result);
     }
 
+    /**
+     * Performs an OR operation between two bits and stores the result in a third bit.
+     *
+     * @param b1 The first {@code Bit} operand.
+     * @param b2 The second {@code Bit} operand.
+     * @param result The {@code Bit} where the result will be stored.
+     */
     public static void or(Bit b1, Bit b2, Bit result) {
         if((b1.getValue() == boolValues.TRUE) || (b2.getValue() == boolValues.TRUE)) {
             result.assign(boolValues.TRUE);
@@ -71,17 +99,17 @@ public class Bit {
         xor(this, b2, result);
     }
 
+    /**
+     * Performs an XOR operation between two bits and stores the result in a third bit.
+     *
+     *
+     *
+     * @param b1 The first {@code Bit} operand.
+     * @param b2 The second {@code Bit} operand.
+     * @param result The {@code Bit} where the result will be stored.
+     */
     public static void xor(Bit b1, Bit b2, Bit result) {
         if(((b1.getValue() == boolValues.TRUE) || (b2.getValue() == boolValues.TRUE)) && (b1.getValue() != b2.getValue())) {
-            result.assign(boolValues.TRUE);
-        }
-        else {
-            result.assign(boolValues.FALSE);
-        }
-    }
-
-    public static void not(Bit b2, Bit result) {
-        if(b2.getValue() == boolValues.FALSE) {
             result.assign(boolValues.TRUE);
         }
         else {
@@ -93,13 +121,28 @@ public class Bit {
         not(this, result);
     }
 
+    /**
+     * Performs a NOT operation on the given bit and stores the result in another bit.
+     *
+     * @param b2 The {@code Bit} operand to be inverted.
+     * @param result The {@code Bit} where the result will be stored.
+     */
+    public static void not(Bit b2, Bit result) {
+        if(b2.getValue() == boolValues.FALSE) {
+            result.assign(boolValues.TRUE);
+        }
+        else {
+            result.assign(boolValues.FALSE);
+        }
+    }
 
-    /*
-    Name: toString
-    Parameters: N/A
-    Takes the private member single_bit and uses a switch statement to see if its TRUE or FALSE
-    if it is TRUE then it returns a single lowercase "f"
-    if it is FALSE then it returns a single lowercase "t"
+    /**
+     * Converts the private member {@code single_bit} from the {@code boolValues} enum to a string representation (either "t" or "f")
+     * Uses a switch statement to check if the value in {@code single_bit} is either {@code TRUE} or {@code FALSE}
+     *
+     * @return "t" if {@code single_bit} is {@code TRUE},
+     *         "f" if {@code single_bit} is {@code FALSE}
+     *          otherwise returns an error message to the user
      */
     public String toString() {
         switch(single_bit) {
