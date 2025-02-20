c071a9a3a93f75254bf7b7c209c325c1349eee8a
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-02-19 20:02:16 -0500

    multiplier done, passes all tests although there could be edge cases

--- src/Multiplier.java
+++ src/Multiplier.java
@@ -1,4 +1,21 @@
 public class Multiplier {
-    public static void multiply(Word32 a, Word32 b, Word32 result) { //two 32 bit numbers will equal 64 bits but we will be ignoring the top 32 bits
+    public static void multiply(Word32 a, Word32 b, Word32 result) {//two 32 bit numbers will equal 64 bits but we will be ignoring the top 32 bits
+
+        int shifter = 0;
+        for(int i = 31; i >= 0; i--) {
+            Bit geta = new Bit(false);
+            Word32 add_to_result = new Word32();
+            Word32 add_to_result_shifted = new Word32(); //have to make a seperate value for this because of the way shift works
+            a.getBitN(i, geta); //geta now has the bit we are using as the multiplier
+            if(geta.getValue() == Bit.boolValues.TRUE)// if it is one we will copy b into add to result shift it by the shifter
+            {
+                b.copy(add_to_result);//add to result now holds b
+                Shifter.LeftShift(add_to_result, shifter, add_to_result_shifted); //shift the value
+                Adder.add(result, add_to_result_shifted, result);//add to result now holds the shifted value
+            }
+
+            shifter++;
+
+        }
     }
 }
