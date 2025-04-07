a00f46e1af83f7ad526c895e6da8ec6e6655ae69
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-04-07 16:04:46 -0400

    processor initial. need to ask more questions about what the PC vs address means

--- src/Processor.java
+++ src/Processor.java
@@ -7,14 +7,57 @@ public class Processor {
     private Memory mem;
     public List<String> output = new LinkedList<>();
 
+    private int PC; //program counter
+
+    private Word16 current_instruction;
+
+    private boolean fetch_flag;
+
+    private int address;
+
+
+
+
+
     public Processor(Memory m) {
         mem = m;
+        PC = 0;
+        current_instruction = new Word16();
+        fetch_flag = false;
+        address = 0;
     }
 
     public void run() {
+        while (true) { //loops through fetch decode execute store
+            fetch();
+            decode();
+            execute();
+            store();
+            //TODO: add in logic that checks for a "halt" to run if found then break
+        }
+
     }
 
-    private void fetch() {
+    private void fetch() { //only read every 2 fetch calls
+
+        if(PC % 2 == 0)
+        {
+
+            mem.read();
+            if(PC != 0)
+            {
+                PC++;
+            }
+            fromInt(PC, mem.address);
+            mem.value.getTopHalf(current_instruction);
+
+
+        }
+        else
+        {
+            PC++;
+            mem.value.getBottomHalf(current_instruction);
+        }
     }
 
     private void decode() {
@@ -46,5 +89,18 @@ public class Processor {
     }
 
     private void store() {
+    }
+
+
+    private void fromInt(int value, Word32 result) {
+        for(int i = 31; i >= 0; i--)
+        {
+            boolean Bit_val = (value & 1) == 1; //Determining the least significant bit of the int (ex 13 (1101) will isolate 1). this can then be checked
+
+            result.setBitN(i, new Bit(Bit_val)); //Setting the bit in the same position as the one we are checking
+
+            value >>= 1 ; //right shift the value to "drop" the LSB. allowing us to work with the next digit (ex 13 >> 1 = 0110) now what was the second digit
+            //is the new LSB so on the next iteration we can determine the value to place in the correct position
+        }
     }
 }
