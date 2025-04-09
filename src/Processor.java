5442d22798357498192400d6c9f7c804d922dd0c
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-04-08 21:15:37 -0400

    first draft of decode. seems to work for the testadd up until the moment i stopped coding which is typically a good sign. hope for the best i suppose

--- src/Processor.java
+++ src/Processor.java
@@ -1,42 +1,45 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Processor {
+
+    private Set<Integer> call_return_opcodes;
     private Memory mem;
     public List<String> output = new LinkedList<>();
-
     private ALU alu;
-
     private int PC; //program counter
-
-
     private Word16 current_instruction;
-
-
     private boolean halt_found;
-
-
-
     private Word32 op1;
     private Word32 op2;
-
     Bit instruction_cycle;
-
-
-
+    Word32[] registers;
+    private Stack<Integer> callStack;
 
 
     public Processor(Memory m) {
-        mem = m;
-        PC = 0;
-        current_instruction = new Word16();
-        halt_found = false;
-        instruction_cycle = new Bit(false);
-        op1 = new Word32();
-        op2 = new Word32();
-        alu = new ALU();
+        mem = m; //memory
+        PC = 0; //PC
+        current_instruction = new Word16(); //the current instruction were working with
+        halt_found = false; //flag for when a halt is found
+        instruction_cycle = new Bit(true); //Bit for knowing when to read a new instruction
+        op1 = new Word32(); //op1 for alu
+        op2 = new Word32(); //op2 for alu
+        alu = new ALU(); //ALU
+        registers = new Word32[32]; //array to hold the registers
+        for(int i = 0; i < 32; i++) { //init registers to 0
+            registers[i] = new Word32();
+        }
+        callStack = new Stack<>(); //stack
+        call_return_opcodes = new HashSet<>(){{
+            add(9);
+            add(10);
+            add(12);
+            add(13);
+            add(14);
+            add(15);
+            add(16);
+            add(17);
+        }};
     }
 
     public void run() {
@@ -48,14 +51,13 @@ public class Processor {
             if(halt_found) {
                 break;
             }
-            //TODO: add in logic that checks for a "halt" to run if found then break
         }
 
     }
 
     private void fetch() { //only read every 2 fetch calls
 
-        if(instruction_cycle.getValue() == Bit.boolValues.FALSE)
+        if(instruction_cycle.getValue() == Bit.boolValues.TRUE)
         {
 
             mem.read();
@@ -72,30 +74,69 @@ public class Processor {
         instruction_cycle.not(instruction_cycle);
     }
 
-    private void decode() {
+    private void decode() { //get everything ready
         //check between call return and 2r/immediate
         int opcode = getOpcode();
-        //
+        if(call_return_opcodes.contains(opcode)) {
+            for(int i = 15; i >= 5; i--)
+            {
+                Bit temp = new Bit(true);
+                current_instruction.getBitN(i, temp);
+                op1.setBitN(i+16, temp);
+            }
+        }
+        else
+        {
+            Bit flag = new Bit(false);
+            current_instruction.getBitN(5, flag);
+            if(flag.getValue() == Bit.boolValues.TRUE) //immediate
+            {
+                for(int i = 10; i >= 6; i--)
+                {
+                    Bit temp = new Bit(true);
+                    current_instruction.getBitN(i, temp);
+                    op2.setBitN(i+21, temp);
+                }
+
+                int reg_index = 0;
+                int pow = 0;
+
+                reg_index = getRegister2(reg_index, pow);
+
+                registers[reg_index].copy(op1); //getting the value in the second register
+
+            }
+            else { //2R
+                int[] register_indices = registers_as_int();
+                int first_register = register_indices[0];
+                int second_register = register_indices[1];
+                registers[second_register].copy(op1);
+                registers[first_register].copy(op2);
+            }
+        }
+
 
 
         switch (opcode) {
-            case 1, 2, 3, 4, 5, 6, 7, 11, 19, 20 -> { //ALU methods
-
+            case 1, 2, 3, 4, 5, 6, 7, 11 -> { //ALU methods (need to set the alu members in order to run do instruction in execute
+                op1.copy(alu.op1);
+                op2.copy(alu.op2);
+                current_instruction.copy(alu.instruction);
             }
-            case 0,10, 12, 13, 14, 15, 16, 17, 18 -> {
-
+            case 0 ->
+            {
+                halt_found = true;
             }
-
         }
-
     }
 
     private void execute() {
+        //do the work
     }
 
     private void printReg() {
         for (int i = 0; i < 32; i++) {
-            var line = "r"+ i + ":" + ""; // TODO: add the register value here...
+            var line = "r"+ i + ":" + registers[i].toString(); // TODO: add the register value here...
             output.add(line);
             System.out.println(line);
         }
@@ -116,6 +157,7 @@ public class Processor {
     }
 
     private void store() {
+        //move stuff around
     }
 
 
@@ -140,5 +182,42 @@ public class Processor {
 
         return opcode_as_int;
     }
+
+    public int[] registers_as_int() {
+        int register_1 = 0;
+        int register_2 = 0;
+        int pow = 0;
+
+        for (int i = 10; i >= 6; i--) {
+            Bit cur_bit = new Bit(false);
+            current_instruction.getBitN(i, cur_bit);
+            if (cur_bit.getValue() == Bit.boolValues.TRUE) {
+                register_1 |= (1 << pow);
+            }
+            pow++;
+        }
+
+        // Reset power for register_2
+        pow = 0;
+
+        // Extract register_2: bits 11 to 15 (inclusive)
+        register_2 = getRegister2(register_2, pow);
+
+        return new int[] { register_1, register_2 };
+
+    }
+
+    private int getRegister2(int register_2, int pow) {
+        for (int i = 15; i >= 11; i--) {
+            Bit cur_bit = new Bit(false);
+            current_instruction.getBitN(i, cur_bit);
+            if (cur_bit.getValue() == Bit.boolValues.TRUE) {
+                register_2 |= (1 << pow);
+            }
+            pow++;
+        }
+        return register_2;
+    }
+
 
 }
